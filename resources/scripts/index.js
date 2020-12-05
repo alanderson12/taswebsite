0f4e2d4df97b910bc6cab95c880b9d459a6a52e3
Abigail Anderson <abigailanderson@Abigails-MacBook-Pro-2.local>
2020-12-04 23:02:31 -0600

    changed get to feedbacks

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -54,7 +54,7 @@ function move() {
 
 function getFeedback(){
   //const m = "https://localhost:5001/api/feedback/"
-  const allFeedbackApiUrl = "https://tasapi.herokuapp.com/api/feedback"
+  const allFeedbackApiUrl = "https://tasapi.herokuapp.com/api/feedbacks"
 
   fetch(m).then(function(response){
       return response.json();
